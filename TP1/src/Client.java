9011226222f776da3982434259029e5fe09a029b
Ruben Almeida <almeidaruben35@gmail.com>
2021-02-23 14:04:26 +0000

    Client Refactored

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Client {
     private static DatagramSocket socket;
     private static InetAddress address;
     private static int port;
-    private static int returnCode;
+    private static String operation;
 
     public static void main(String[] args) {
 
@@ -17,57 +16,61 @@ public class Client {
             System.out.println("Error opening Socket");
         }
 
-
-        if(args.length<3){
+        if (args.length < 3) {
             System.out.println("Minimal number of arguments not reached");
             return;
         }
 
-
         try {
-            address=InetAddress.getByName(args[0]);
+            address = InetAddress.getByName(args[0]);
         } catch (UnknownHostException e) {
             System.out.println("Host not reachable");
-            return;
         }
         try {
-            port=Integer.parseInt(args[1]);
-        }catch (NumberFormatException exception){
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException exception) {
             System.out.println("Argument 1 of input not valid");
         }
 
-        if(args[2].equals("register"))
-            handleRegister(args);
-        else if(args[2].equals("lookup"))
-            handleLookup(args);
-        else
+        String stringToSend;
+        operation=args[2];
+
+        if (operation.equals("register"))
+            stringToSend=handleRegisterRequestString(args);
+        else if (operation.equals("lookup"))
+            stringToSend=handleLookupRequestString(args);
+        else{
             System.out.println("Invalid Operation");
+            return;
+        }
+
+        if(stringToSend==null)
+            return;
+
+        sendMessage(stringToSend);
+
+        processReply(receiveReply());
 
 
         socket.close();
 
     }
 
-    public static void handleRegister(String[] args){
+    public static void sendMessage(String stringToSend){
 
-        if(args.length!=5){
-            System.out.println("Invalid number of arguments of a register request");
-            return;
-        }
-        String dnsName=new String(args[3]);
-        String IPAddress=new String(args[4]);
-
-        String string="register "+dnsName+" "+IPAddress;
-        byte[] sendingBuf=string.getBytes();
+        byte[] sendingBuf = stringToSend.getBytes();
 
         try {
-            socket.send(new DatagramPacket(sendingBuf,sendingBuf.length,address,port));
+            socket.send(new DatagramPacket(sendingBuf, sendingBuf.length, address, port));
         } catch (IOException e) {
             System.out.println("Error Sending register attempt");
         }
+    }
+    public static String receiveReply(){
 
-        byte[] receivingBuf=new byte[512];
-        DatagramPacket response=new DatagramPacket(receivingBuf,receivingBuf.length,address,port);
+        byte[] receivingBuf = new byte[512];
+
+        DatagramPacket response = new DatagramPacket(receivingBuf, receivingBuf.length, address, port);
 
         try {
             socket.receive(response);
@@ -75,51 +78,43 @@ public class Client {
             System.out.println("Error while receiving reply from register");
         }
 
-        String reply=new String(response.getData());
-        String[] tokenReplies=reply.split(" ");
+        return new String(response.getData());
 
-        String genericClientMessage="Client: register " + dnsName+" "+ IPAddress;
-
-        if(tokenReplies[0].equals("-1"))
-            System.out.println(genericClientMessage+" : " +"ERROR");
-        else
-            System.out.println(genericClientMessage+" : " + returnCode);
     }
 
-    public static void handleLookup(String[] args){
+    public  static void processReply(String reply){
 
-        if(args.length!=4){
-            System.out.println("Invalid number of arguments lookup request");
-            return;
-        }
-        String dnsName=new String(args[3]);
+        String[] tokenReplies = reply.split(" ");
 
-        String string="lookup "+dnsName;
-        byte[] sendingBuf=string.getBytes();
+        String genericClientMessage = "Client: "+ operation+" " + tokenReplies[1] + " " + tokenReplies[2];
 
-        try {
-            socket.send(new DatagramPacket(sendingBuf,sendingBuf.length,address,port));
-        } catch (IOException e) {
-            System.out.println("Error Sending register attempt");
-        }
-
-        byte[] receivingBuf=new byte[512];
-        DatagramPacket response=new DatagramPacket(receivingBuf,receivingBuf.length,address,port);
-
-        try {
-            socket.receive(response);
-        } catch (IOException e) {
-            System.out.println("Error while receiving reply from register");
-        }
-
-        String reply=new String(response.getData());
-        String[] tokenReplies=reply.split(" ");
-
-        String genericClientMessage="Client: lookup " + dnsName;
-
-        if(tokenReplies[0].equals("-1"))
-            System.out.println(genericClientMessage+" : " +"ERROR");
+        if (tokenReplies[0].equals("-1"))
+            System.out.println(genericClientMessage + " : " + "ERROR");
         else
-            System.out.println(genericClientMessage+" : " + returnCode);
+            System.out.println(genericClientMessage + " : " + tokenReplies[0]);
+
+    }
+
+    public static String handleRegisterRequestString(String[] args) {
+
+        if (args.length != 5) {
+            System.out.println("Invalid number of arguments of a register request");
+            return null;
+        }
+        String dnsName = args[3];
+        String IPAddress = args[4];
+
+        return ("register " + dnsName + " " + IPAddress);
+    }
+
+    public static String handleLookupRequestString(String[] args) {
+
+        if (args.length != 4) {
+            System.out.println("Invalid number of arguments lookup request");
+            return null;
+        }
+        String dnsName = args[3];
+
+        return "lookup " + dnsName;
     }
 }
--- TP1/src/Server.java
+++ TP1/src/Server.java
@@ -1,27 +1,24 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Inet4Address;
 import java.net.SocketException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Server {
-    final private static int messageSize=512;
-    private static int port=-1;
+    final private static int messageSize = 512;
+    private static int port = -1;
     private static DatagramSocket socket;
-    private static HashMap<String,String> dnsTable;
+    private static HashMap<String, String> dnsTable;
 
     public static void main(String[] args) {
 
-        if(args.length!=1){
+        if (args.length != 1) {
             System.out.println("Input Arguments not valid");
             return;
         }
         try {
-            port=Integer.parseInt(args[0]);
-        }catch (NumberFormatException exception){
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException exception) {
             System.out.println("Argument 1 of input not valid");
         }
 
@@ -32,7 +29,7 @@ public class Server {
             System.out.println("Error opening Socket");
         }
 
-        dnsTable=new HashMap<>();
+        dnsTable = new HashMap<>();
 
         try {
             run();
@@ -43,65 +40,65 @@ public class Server {
         socket.close();
 
     }
-    public static void run() throws IOException {
-        byte[] buf= new byte[messageSize];
 
-        DatagramPacket packet = new DatagramPacket(buf,messageSize);
+    public static void run() throws IOException {
+        byte[] buf = new byte[messageSize];
+
+        DatagramPacket packet = new DatagramPacket(buf, messageSize);
 
         System.out.println("Server Running");
 
-        while (true){
+        while (true) {
 
             socket.receive(packet);
-            String request= new String(packet.getData());
+            String request = new String(packet.getData());
 
 
-            System.out.println("Server:"+request);
+            System.out.println("Server:" + request);
 
-            reply(request,packet);
+            reply(request, packet);
 
             packet.setData(new byte[512]);
 
         }
     }
 
-    public static void reply(String request,DatagramPacket packet){
-       String[] tokenParsedRequest=request.split(" ");
+    public static void reply(String request, DatagramPacket packet) {
+        String[] tokenParsedRequest = request.split(" ");
 
-       if(tokenParsedRequest.length<2){
-           System.out.println("Not well formatted Request");
-           return;
-       }
-       if(tokenParsedRequest[0].equals("register")){
+        if (tokenParsedRequest.length < 2) {
+            System.out.println("Not well formatted Request");
+            return;
+        }
+        if (tokenParsedRequest[0].equals("register")) {
 
-           if(tokenParsedRequest.length!=3){
-               System.out.println("Register Request MalFormed");
-               return;
-           }
-           dnsTable.put(tokenParsedRequest[1],tokenParsedRequest[2]);
-           String toReply=dnsTable.size()-1+"\n"+tokenParsedRequest[1]+tokenParsedRequest[2];
+            if (tokenParsedRequest.length != 3) {
+                System.out.println("Register Request MalFormed");
+                return;
+            }
+            dnsTable.put(tokenParsedRequest[1], tokenParsedRequest[2]);
+            String toReply = dnsTable.size() - 1 + " " + tokenParsedRequest[1] + " " + tokenParsedRequest[2];
 
-           packet.setData(toReply.getBytes());
-           try {
-               socket.send(packet);
-           } catch (IOException e) {
-               System.out.println("Error sending Reply");
-           }
-       }
-       else if(tokenParsedRequest[0].equals("lookup")){
-           if(tokenParsedRequest.length!=2){
-               System.out.println("Lookup Request MalFormed");
-               return;
-           }
-           String IPToReturn=dnsTable.get(tokenParsedRequest[1]);
-           String toReply= dnsTable.size() + " " + tokenParsedRequest[1] + " " + IPToReturn;
-           packet.setData(toReply.getBytes());
-           try {
-               socket.send(packet);
-           } catch (IOException e) {
-               System.out.println("Error sending Reply");
-           }
-       }
+            packet.setData(toReply.getBytes());
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                System.out.println("Error sending Reply");
+            }
+        } else if (tokenParsedRequest[0].equals("lookup")) {
+            if (tokenParsedRequest.length != 2) {
+                System.out.println("Lookup Request MalFormed");
+                return;
+            }
+            String IPToReturn = dnsTable.get(tokenParsedRequest[1]);
+            String toReply = dnsTable.size() + " " + tokenParsedRequest[1] + " " + IPToReturn;
+            packet.setData(toReply.getBytes());
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                System.out.println("Error sending Reply");
+            }
+        }
     }
 
 }
