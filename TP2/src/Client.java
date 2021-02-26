0d60fa4d2fcf3ee722e4b62ca65b7c6edbffd0c7
Ruben Almeida <almeidaruben35@gmail.com>
2021-02-26 23:29:59 +0000

    Server parsing input. Client initial code

--- TP2/src/Client.java
+++ TP2/src/Client.java
@@ -1,5 +1,108 @@
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
+public class Client {
+    private static DatagramSocket unicastSocket;
+    private static MulticastSocket multicastSocket;
+
+    public static void main(String[] args) {
+        if (args.length == 5 && !processInputRegister(args)) {
+            System.out.println("Error");
+            return;
+        } else if (args.length == 4 && !processInputLookup(args)) {
+            System.out.println("Error");
+            return;
+        } else {
+            System.out.println("Number of parameters invalid");
+            return;
+        }
     }
+
+    private static boolean processInputLookup(String[] args) {
+
+        if (!multiCastInitializer(args[0], args[1]))
+            return false;
+
+        if (!uniCastInitializer())
+            return false;
+
+        return true;
+    }
+
+    private static boolean processInputRegister(String[] args) {
+
+        if (!multiCastInitializer(args[0], args[1]))
+            return false;
+        if (!uniCastInitializer())
+            return false;
+        return true;
+    }
+
+
+    private static boolean multiCastInitializer(String multiCastAddress, String strPort) {
+
+        int multicastPort;
+
+        try {
+            multicastPort = Integer.parseInt(strPort);
+        } catch (NumberFormatException exception) {
+            System.out.println("Argument 1 of input not valid");
+            return false;
+        }
+
+        try {
+            multicastSocket = new MulticastSocket(multicastPort);
+        } catch (IOException e) {
+            System.out.println("Error opening Socket");
+            return false;
+        }
+        try {
+            multicastSocket.joinGroup(InetAddress.getByName(multiCastAddress));
+        } catch (IOException e) {
+            System.out.println("Error joining the Group");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean uniCastInitializer() {
+
+
+        int unicastPort;
+
+        try {
+            //unicastPort = Integer.parseInt();
+        } catch (NumberFormatException exception) {
+            System.out.println("Argument 1 of input not valid");
+            return false;
+        }
+
+        return true;
+    }
+        /*
+    public static boolean processInput(String[] args) {
+
+
+
+        try {
+            unicastPort = Integer.parseInt(args[0]);
+        } catch (NumberFormatException exception) {
+            System.out.println("Argument 1 of input not valid");
+            return false;
+        }
+        int unicastPort;
+
+
+        try {
+            unicastSocket = new DatagramSocket(unicastPort);
+        } catch (SocketException e) {
+            System.out.println("Error opening Socket");
+            return false;
+        }
+
+        return true;
+    }
+         */
 }
--- TP2/src/Server.java
+++ TP2/src/Server.java
@@ -1,5 +1,69 @@
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
 
+//224.0.0.0 to 239.255.255.255 Range UDP Multicast
+public class Server {
+    private static HashMap<String, String> dnsTable;
+    private static DatagramSocket unicastSocket;
+    private static MulticastSocket multicastSocket;
+
+    public static void main(String[] args) {
+        dnsTable = new HashMap<>();
+
+        if (args.length != 3) {
+            System.out.println("Incorrect Number of arguments");
+            return;
+        }
+        if (!processInput(args)) {
+            System.out.println("Error processing Input");
+            return;
+        }
+
+
+    }
+
+    public static boolean processInput(String[] args) {
+        int unicastPort;
+        int multicastPort;
+        String multiCastAddress;
+
+        try {
+            unicastPort = Integer.parseInt(args[0]);
+        } catch (NumberFormatException exception) {
+            System.out.println("Argument 1 of input not valid");
+            return false;
+        }
+
+        multiCastAddress = args[1];
+
+        try {
+            multicastPort = Integer.parseInt(args[2]);
+        } catch (NumberFormatException exception) {
+            System.out.println("Argument 1 of input not valid");
+            return false;
+        }
+
+        try {
+            unicastSocket = new DatagramSocket(unicastPort);
+        } catch (SocketException e) {
+            System.out.println("Error opening Socket");
+            return false;
+        }
+        try {
+            multicastSocket = new MulticastSocket(multicastPort);
+        } catch (IOException e) {
+            System.out.println("Error opening Socket");
+            return false;
+        }
+
+        try {
+            multicastSocket.joinGroup(InetAddress.getByName(multiCastAddress));
+        } catch (IOException e) {
+            System.out.println("Error joining the Group");
+            return false;
+        }
+
+        return true;
     }
 }
